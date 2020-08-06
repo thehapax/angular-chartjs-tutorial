d750ebf6e411f1460f9fcc2de4719424806f4725
thehapax <39811582+thehapax@users.noreply.github.com>
2020-08-06 00:45:11 -0700

    add header, add line-chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineChartComponent
+    LineChartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-line-chart',
@@ -10,6 +13,39 @@ export class LineChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Product A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Product B' }
+  ];
+
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  lineChartOptions: ChartOptions = {
+    responsive: true
+  };
+  lineChartColors: Color[] = [
+
+    {
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+    },
+    {
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+    }
+  ];
+
+  lineChartLegend = true;
+  lineChartType = 'line';
+  lineChartPlugins = [];
+
+  chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
 }
